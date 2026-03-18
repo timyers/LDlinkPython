c46c047472b9d8498d8a1ee3df36936c08fffbbd
timyers <48717893+timyers@users.noreply.github.com>
2026-03-18 09:00:53 -0400

    update version number following initial tag

--- ldlinkpython/__init__.py
+++ ldlinkpython/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 DEFAULT_API_ROOT = "https://ldlink.nih.gov/LDlinkRest"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from ldlinkpython.client import LDlinkClient
 from ldlinkpython.endpoints.ldpair import ldpair
